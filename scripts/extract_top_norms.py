29cb8ca49f859a0ee17744f1d5fa069826a5e990
Stephen Cranefield <suppressed@email.address>
2020-10-08 18:00:54 +1300

    Added metrics/chains_and_posteriors as dependency for analyse_chains.py so it could include log posteriors in precision_recall.txt

--- scripts/extract_top_norms.py
+++ scripts/extract_top_norms.py
@@ -11,6 +11,8 @@ from collections import Counter
 import operator
 import pickle
 import pickle_wrapper
+import pandas as pd
+import ast
 
 with open("params.yaml", 'r') as fd:
     params = yaml.safe_load(fd)
@@ -26,18 +28,28 @@ true_expression = params['true_norm']['exp']
 
 posterior_sample = unpickle('data/posterior.pickle')
 learned_expressions=Counter(map(to_tuple, posterior_sample))
-n = 5
+n = 20
 top_norms_with_freq = learned_expressions.most_common(n)
 top_norms = list(map(operator.itemgetter(0), top_norms_with_freq))
 pickle_it(top_norms, 'data/top_norms.pickle')
 
 env = unpickle('data/env.pickle')
 
+exp_posterior_df = pd.read_csv('metrics/chain_posteriors.csv', usecols=['expression','log_posterior'])
+exp_posterior_df = exp_posterior_df.drop_duplicates()
+exp_posterior_df['post_rank'] = exp_posterior_df['log_posterior'].rank(method='dense',ascending=False)
+exp_posterior_df.sort_values('post_rank', inplace=True)
+exp_posterior_df['expression'] = exp_posterior_df['expression'].transform(ast.literal_eval)
+exp_posterior_df['expression'] = exp_posterior_df['expression'].transform(to_tuple)
+
+def log_posterior(exp, exp_lp_df):
+    return exp_lp_df.loc[exp_lp_df['expression'] == exp]['log_posterior'].iloc[0]
+
 with open('metrics/precision_recall.txt', 'w') as f:
     f.write("Number of unique Norms in sequence={len(learned_expressions)}\n")
     f.write(f"Top {n} norms:\n")
     for expression,freq in top_norms_with_freq:
-        f.write(f"Freq. {freq}: ")
+        f.write(f"Freq. {freq}, lp {log_posterior(expression, exp_posterior_df)}: ")
         f.write(f"{expression}\n")
     f.write("\n")
 
