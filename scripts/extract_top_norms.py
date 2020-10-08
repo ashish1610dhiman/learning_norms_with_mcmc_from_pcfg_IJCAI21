ebdc9b8c8fbb150e4ea1b98bfbe96bd35cecaf57
Stephen Cranefield <suppressed@email.address>
2020-10-08 23:26:51 +1300

    Changed extract_top_norms.py to use 100000 random seqs. for precision and recall

--- scripts/extract_top_norms.py
+++ scripts/extract_top_norms.py
@@ -56,7 +56,7 @@ with open('metrics/precision_recall.txt', 'w') as f:
 # Calculate precision and recall of top_n norms from learned expressions
 pr_result=performance(the_task,env,true_expression,learned_expressions,
                         folder_name="temp",file_name="top_norm",
-                        top_n=n,beta=1,repeat=50000,verbose=False)
+                        top_n=n,beta=1,repeat=100000,verbose=False)
 
 with open('metrics/precision_recall.txt', 'a') as f:
     f.write(pr_result.to_string())
